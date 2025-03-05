563519d8a040d3f1eff6e1ebc3ca5d87d09f6592
Eric Johnson <ericjohnson8055@gmail.com>
2025-03-05 16:05:39 -0600

    ch1

--- chapter_1.py
+++ chapter_1.py
@@ -4,11 +4,28 @@ fn = "Darius-13-100m-Fly.txt"
 
 # help(fn.split)
 
-print(fn.split("-"))
+# print(fn.split("-"))
 
-name = fn.split("-")[0]
-age = fn.split("-")[1]
-distance = fn.split("-")[2]
-stroke = fn.split("-")[3]
+# name = fn.split("-")[0]
+# age = fn.split("-")[1]
+# distance = fn.split("-")[2]
+# stroke = fn.split("-")[3]
 
-print(name, age, distance, stroke)
+# print(name, age, distance, stroke)
+
+help(fn.removesuffix)
+
+print(fn.removesuffix(".txt"))
+
+"Fly.txt".removesuffix(".txt")
+"Free.txt".removesuffix(".txt")
+"Back.txt".removesuffix(".txt")
+"Breast.txt".removesuffix(".txt")
+
+swimmer, age, distance, stroke = fn.removesuffix(".txt").split("-")
+print(swimmer, age, distance, stroke)
+
+
+fn = "Abi-10-50m-Back.txt"
+swimmer, age, distance, stroke = fn.removesuffix(".txt").split("-")
+print(swimmer, age, distance, stroke)
---
+++ scratch.py
@@ -0,0 +1,11 @@
+my_list = [1, 2, 3, 4, 5]
+
+print(dir(my_list))
+
+help(my_list.remove)
+
+my_list.remove(3)
+print(my_list)
+
+my_list.remove(3)
+print(my_list)
